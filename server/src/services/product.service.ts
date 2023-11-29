cd04c4872313b6d28ce01c75538e8e735d39ba73
mikle <zimboroda@gmail.com>
2023-11-29 13:56:24 +0200

    add creating product

--- server/src/services/product.service.ts
+++ server/src/services/product.service.ts
@@ -6,6 +6,8 @@ import {FilesService, FilesServiceInterface} from "./files.service";
 
 export interface ProductServiceInterface {
     updateProduct(id: string, product: Product): Promise<void>;
+
+    createProduct(product: Omit<Product, 'id'>): Promise<Product>;
 }
 
 @injectable({scope: BindingScope.TRANSIENT})
@@ -16,6 +18,20 @@ export class ProductService implements ProductServiceInterface {
 
         @repository(ProductRepository)
         public productRepository: ProductRepository) {
+    }
+
+    async createProduct(product: Product) {
+        const productImage = product?.previewImage;
+
+        if (productImage) {
+            const filename = await this.fileService.uploadFile(productImage);
+            return await this.productRepository.create({
+                ...product,
+                previewImage: filename,
+            });
+        }
+
+        return await this.productRepository.create(product);
     }
 
     async updateProduct(id: string, product: Product) {
